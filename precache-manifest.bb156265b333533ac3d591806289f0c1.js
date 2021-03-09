4af7642024084c2007a3bcb8ec68639145cc7100
yzkim9501 <yzkim9501@naver.com>
2021-03-09 17:09:59 +0900

    Updates

---
+++ precache-manifest.bb156265b333533ac3d591806289f0c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9aea22b2f03b07df9de56bbd57240cd",
+    "url": "/PFolio/index.html"
+  },
+  {
+    "revision": "0895e22b34f50e9504bd",
+    "url": "/PFolio/static/css/main.f77d7a38.chunk.css"
+  },
+  {
+    "revision": "fe6ba093163001d5f2e3",
+    "url": "/PFolio/static/js/2.a9eb2ed6.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/PFolio/static/js/2.a9eb2ed6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0895e22b34f50e9504bd",
+    "url": "/PFolio/static/js/main.55721fc8.chunk.js"
+  },
+  {
+    "revision": "a623957e406771c9a466",
+    "url": "/PFolio/static/js/runtime-main.b10a1cca.js"
+  },
+  {
+    "revision": "b88924d5d914bf9f3885f87c2ed76477",
+    "url": "/PFolio/static/media/sad-icon.b88924d5.png"
+  },
+  {
+    "revision": "b9d7d448a09d6aacb45d21aab2c6fd53",
+    "url": "/PFolio/static/media/your photo.b9d7d448.png"
+  }
+]);
